75691211f8c9b5ffa33317ec0a742dec7ecd8d60
pro123908 <pro123908@gmail.com>
2018-09-12 19:22:56 +0500

    login/Register/Logout

---
+++ client/src/validation/isEmpty.js
@@ -0,0 +1,8 @@
+//Function to check if given data structure is empty of not?
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+export default isEmpty;
