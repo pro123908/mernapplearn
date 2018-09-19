e9de3b480a79e0b422bcc91c5a6c1fadb3c1f05b
pro123908 <pro123908@gmail.com>
2018-09-19 14:48:10 +0500

    Profile Complete

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 import store from "./store";
 
@@ -83,6 +84,7 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
